47ba28b21eab31e59cdebc7f26fe54f4a9a9b6f2
Paul Hein <dehein@users.noreply.github.com>
2018-10-04 13:37:21 +0200

    add all the good stuff into main.py

--- src/main.py
+++ src/main.py
@@ -23,4 +23,60 @@ UNKNOWN_POSITION = -1  # initial state so we know if something went wrong
 rotary_prev_state = [rot_a.value, rot_b.value]
 
 while True:
-  
+    # reset encoder and wait for the next turn
+    encoder_direction = 0
+ 
+    # take a 'snapshot' of the rotary encoder state at this time
+    rotary_curr_state = [rot_a.value, rot_b.value]
+    print(rotary_curr_state)
+    # if rotary_curr_state != rotary_prev_state:
+    #     #print("Changed")
+    #     if rotary_prev_state == [True, True]:
+    #         # we caught the first falling edge!
+    #         if not rotary_curr_state[A_POSITION]:
+    #             #print("Falling A")
+    #             falling_edge = A_POSITION
+    #         elif not rotary_curr_state[B_POSITION]:
+    #             #print("Falling B")
+    #             falling_edge = B_POSITION
+    #         else:
+    #             # uhh something went deeply wrong, lets start over
+    #             continue
+ 
+    #     if rotary_curr_state == [True, True]:
+    #         # Ok we hit the final rising edge
+    #         if not rotary_prev_state[B_POSITION]:
+    #             rising_edge = B_POSITION
+    #             # print("Rising B")
+    #         elif not rotary_prev_state[A_POSITION]:
+    #             rising_edge = A_POSITION
+    #             # print("Rising A")
+    #         else:
+    #             # uhh something went deeply wrong, lets start over
+    #             continue
+ 
+    #         # check first and last edge
+    #         if (rising_edge == A_POSITION) and (falling_edge == B_POSITION):
+    #             encoder_counter -= 1
+    #             encoder_direction = -1
+    #             print("%d dec" % encoder_counter)
+    #         elif (rising_edge == B_POSITION) and (falling_edge == A_POSITION):
+    #             encoder_counter += 1
+    #             encoder_direction = 1
+    #             print("%d inc" % encoder_counter)
+    #         else:
+    #             # (shrug) something didn't work out, oh well!
+    #             encoder_direction = 0
+ 
+    #         # reset our edge tracking
+    #         rising_edge = falling_edge = UNKNOWN_POSITION
+ 
+    # rotary_prev_state = rotary_curr_state
+    # # Check if rotary encoder went up
+    # if encoder_direction == 1:
+    #     cc.send(ConsumerControlCode.VOLUME_INCREMENT)
+    # if encoder_direction == -1:
+    #     cc.send(ConsumerControlCode.VOLUME_DECREMENT)
+
+    time.sleep(0.1)
+    
